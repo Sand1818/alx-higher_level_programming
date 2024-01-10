7c362f30096f5faa119ee914b89e269c18ab1ba6
Sandiso <sandiso.jele@gmail.com>
2024-01-10 14:38:22 -0800

    8 recta

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
+"""Rectangle classs"""
+
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-"""
-Rectangle Class
-"""
 
 
 class Rectangle(BaseGeometry):
-    """
-    Defines a Rectangle
-    """
+    """Rectangle class definition"""
 
     def __init__(self, width, height):
-        """Rectangle's constructor
-        Args:
-            width (int): default value is 0
-            height (int): default value is 0
-        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
