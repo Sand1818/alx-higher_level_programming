17fb4a0c2240ee5c8a5961953a87757dfcc2f15a
Sand1818 <sandiso.jele@gmail.com>
2024-02-14 10:48:45 -0800

    task 6 -10

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Lists California cities
+-- results in asceding order
+SELECT id, name FROM cities WHERE state_id = (
+	SELECT id FROM states
+	WHERE name = "California")
+ORDER BY cities.id ASC;
