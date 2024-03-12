3a2e386aafae92094bb67124f20093aa58ed8f99
Sand1818 <sandiso.jele@gmail.com>
2024-03-12 19:10:29 +0200

    minor fix 3 & 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
     constructor (w, h) {
         if (w > 0 && h > 0) {
             this.width = w;
@@ -17,5 +17,5 @@ module.exports = class Rectangle {
         }
     }
 }
-
+ 
 module.exports = Rectangle;
