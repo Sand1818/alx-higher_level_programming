3c7ee8d5f7f72cb9e558da8db861fe6d7f38a8bd
Sand1818 <140617301+Sand1818@users.noreply.github.com>
2024-03-28 12:11:06 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-let a;
-for (a of myArray) {
-  console.log(a);
-}
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-function add (s, j) {
-    console.log(parseInt(s) + parseInt(j));
-  }
-  add(process.argv[2], process.argv[3]);
