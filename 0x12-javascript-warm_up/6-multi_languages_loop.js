d8f505b4c2b853f69ce0674a888557aab6226bdd
Sand1818 <sandiso.jele@gmail.com>
2024-04-09 01:55:24 +0200

    capital S & whites space fixes

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let a;
 for (a of myArray) {
   console.log(a);
