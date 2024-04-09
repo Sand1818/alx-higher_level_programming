32b090610a8ba08961be565130c6e630ed34bf52
Sand1818 <sandiso.jele@gmail.com>
2024-04-09 21:42:56 +0200

    adding readme and 0-7

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+        if (w > 0 && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+ 
+    print () {
+        let s, k;
+        for (s = 0; s < this.height; s++) {
+            for (k = 0; k < this.width; k++) {
+                process.stdout.write('X');
+            }
+            process.stdout.write('\n');
+        }
+    }
+}
+ 
+module.exports = Rectangle;
