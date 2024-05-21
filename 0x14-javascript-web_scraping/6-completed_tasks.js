6369384e5e84a0f455700d92be7cbf0b5eb1fc52
Sand1818 <sandiso.jele@gmail.com>
2024-05-21 19:15:39 +0200

    fix 6-completed*

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,17 +4,17 @@ const request = require('request');
 request.get(process.argv[2], (error, resopnse, body) => {
   if (error) console.log(error);
   else {
-    const Completed_tasks = {};
+    const completedTasks = {};
     const todos = JSON.parse(body);
     for (const todo of todos) {
       if (todo.completed) {
-        if (todo.userId in Completed_tasks) {
-          Completed_tasks[todo.userId]++;
+        if (todo.userId in completedTasks) {
+          completedTasks[todo.userId]++;
         } else {
-          Completed_tasks[todo.userId] = 1;
+          completedTasks[todo.userId] = 1;
         }
       }
     }
-    console.log(Completed_tasks);
+    console.log(completedTasks);
   }
 });
